0e4dfc41838ed92ef6736673c3a9ebf32fb5b150
tkhiro <innocent0402@gmail.com>
2020-11-08 12:47:11 +0900

    #3 テーブル定義

--- HelpPage/help/models.py
+++ HelpPage/help/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class QuestionType(models.Model):
+    questionType = models.CharField(max_length=10)
+
+class Question(models.Model):
+    question_type = models.ForeignKey(QuestionType, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=500)
+    pub_date = models.DateTimeField('date published')
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=1000)
+    pub_date = models.DateTimeField('date published')
